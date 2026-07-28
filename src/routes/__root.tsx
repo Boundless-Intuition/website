ab7afb437867ae710cb4f91ff00a93725aa422b6
Karanjot Singh <drquark@duck.com>
2026-07-28 10:44:20 +0200

    add vercel analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 import appCss from "../styles.css?url";
 function NotFoundComponent() {
@@ -136,6 +137,7 @@ function RootShell({ children }: { children: ReactNode }) {
       <body>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
         {children}
+        <Analytics />
         <Scripts />
       </body>
     </html>
